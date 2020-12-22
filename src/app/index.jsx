d7069a58664b3c0dcb2492f6b915f150b3fff4ca
Dan Donahue <ddonahue215@gmail.com>
2020-12-22 11:47:35 -0500

    Completed Module 4

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -3,9 +3,9 @@ import { store } from './store';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Dashboard } from './components/Dashboard'
+import { Main } from './components/Main'
 
 ReactDOM.render(
-  <Dashboard/>,
+  <Main/>,
   document.getElementById("app")
 );
